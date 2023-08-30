852234e9d5bb99aaf8dadb18c94bb986cfbb545d
LinaCor <LinaAxx@yandex.ru>
2023-08-30 20:30:06 +0300

    finaly ver 30.08

--- js/hall.js
+++ js/hall.js
@@ -1,29 +1,3 @@
-function sendRequest(method, url, body, func) {
-  const xhr = new XMLHttpRequest();
-  xhr.open(method, url);
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.responseType = 'json';
-
-  xhr.addEventListener('load', () => {
-    if (xhr.status >= 200 && xhr.status < 300) {
-      try {
-        let response = xhr.response;
-        func(response);
-      } catch (error) {
-        console.log('Ошибка загрузки');
-        func(null);
-      }
-    }
-  })
-
-  xhr.addEventListener('error', () => {
-    console.log('Ошибка загрузки');
-    func(null);
-  })
-
-  xhr.send(body);
-}
-
 let seanceData = localStorage.getItem('seance');
 let parseSeanceData = JSON.parse(seanceData);
 
@@ -50,3 +24,4 @@ sendRequest('POST', 'https://jscp-diplom.netoserver.ru/', `event=get_hallConfig&
   wrapper.innerHTML = parseSeanceData.hallConfig;
 
 })
+
