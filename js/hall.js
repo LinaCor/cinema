96cebb2545b785934b0dec427d8f132f62c44e80
LinaCor <LinaAxx@yandex.ru>
2023-08-31 18:35:54 +0300

    add payment.js and various edits

--- js/hall.js
+++ js/hall.js
@@ -15,13 +15,53 @@ costViptFilm.innerText = parseSeanceData.hallPriceVip;
 let wrapper = document.querySelector('.conf-step__wrapper');
 
 sendRequest('POST', 'https://jscp-diplom.netoserver.ru/', `event=get_hallConfig&timestamp=${parseSeanceData.timeStampSeance}&hallId=${parseSeanceData.hallId}&seanceId=${parseSeanceData.seanceId}`, function (response) {
-  console.log(response);
-
-  //if (response) {
-  //  parseSeanceData.hallConfig = response;
-  //}
+  if (response) {
+    parseSeanceData.hallConfig = response;
+  }
 
   wrapper.innerHTML = parseSeanceData.hallConfig;
 
-})
+  let chairs = document.querySelectorAll('.conf-step__row .conf-step__chair');
+  let button = document.querySelector('.acceptin-button');
+  button.setAttribute('disabled', true);
+  button.style.cursor = 'pointer';
+  let chairsSelected = document.querySelectorAll('.conf-step__row .conf-step__chair_selected');
 
+
+  for (let chair of chairs) {
+    chair.style.cursor = 'pointer';
+
+    chair.addEventListener('click', () => {
+      chair.classList.toggle('conf-step__chair_selected');
+
+      chairsSelected = document.querySelectorAll('.conf-step__row .conf-step__chair_selected');
+      if (chairsSelected.length == 0) {
+        button.setAttribute('disabled', true);
+      } else {
+        button.removeAttribute('disabled');
+      }
+    })
+  }
+
+  button.addEventListener('click', () => {
+    document.location = 'payment.html';
+    let selectedChair = [];
+    chairsSelected.forEach((item) => {
+      let rowPlaces = item.closest('.conf-step__row');
+      let rowPlace = Array.from(rowPlaces.parentNode.children).indexOf(rowPlaces) + 1;
+      let chairPlace = Array.from(rowPlaces.children).indexOf(item) + 1;
+      let typeCost;
+      if (item.classList.contains('conf-step__chair_vip')) {
+        typeCost = 'vip';
+      }
+      if (item.classList.contains('conf-step__chair_standart')) {
+        typeCost = 'standart';
+      }
+      selectedChair.push({ row: rowPlace, place: chairPlace, type: typeCost });
+    })
+
+    parseSeanceData.hallConfig = wrapper.innerHTML;
+    parseSeanceData.aboutPlaces = selectedChair;
+    localStorage.setItem('seance', JSON.stringify(parseSeanceData));
+  })
+})
---
+++ js/payment.js
@@ -0,0 +1,3 @@
+let seanceInfo = localStorage.getItem('seance');
+let parseSeanceInfo = JSON.parse(seanceInfo);
+console.log(parseSeanceInfo);
