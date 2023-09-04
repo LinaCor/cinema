b473d5d85f3364264f13c4878218febce5ac35b8
LinaCor <LinaAxx@yandex.ru>
2023-09-04 13:50:53 +0300

    add QRCreator.js and various edits

--- js/ticket.js
+++ js/ticket.js
@@ -9,3 +9,16 @@ let hallFilm = document.querySelector('.ticket__hall');
 hallFilm.innerText = parseTicketInfo.hallName.match(/[0-9]+$/);
 let startFilm = document.querySelector('.ticket__start');
 startFilm.innerText = parseTicketInfo.seanceTime;
+
+function createQR() {
+  let image = document.querySelector('.ticket__info-qr');
+  let div = document.createElement('div');
+  let QRinfo = `Электронный билет на фильм ${filmHead.textContent}, ряд/место: ${rowPlace.textContent} в зале ${hallFilm.textContent}, начало сеанса в ${startFilm.textContent}`;
+  let QRcode1 = QRCreator(QRinfo, { image: 'SVG' });
+
+  div.classList.add('ticket__info-qr');
+  image.replaceWith(div);
+  div.append(QRcode1.result);
+}
+
+document.addEventListener('DOMContentLoaded', createQR);
